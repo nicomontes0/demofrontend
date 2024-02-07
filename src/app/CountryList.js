d4cf49c66e8ea974144b37ec0c42335effd25c41
fmontes1 <fmontes@motus.com>
2024-02-07 14:49:59 -0300

    initial commit

---
+++ src/app/CountryList.js
@@ -0,0 +1,15 @@
+'use client'
+
+import React from 'react'
+
+function CountryList() {
+  return (
+    <section>
+        <ul className='large'>
+            <li key="india" className='pa-10 pl-20'> India </li>
+        </ul>
+    </section>
+  )
+}
+
+export default CountryList
---
+++ src/app/Search.js
@@ -0,0 +1,11 @@
+'use client'
+
+import React from 'react'
+
+function Search() {
+  return (
+    <input style={{color: 'black'}} placeholder='type here...' type='text'/>
+  )
+}
+
+export default Search
